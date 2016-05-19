eba7f9fa930c9404140750e484769630339e6585
Yuan Li <yli@pacificbiosciences.com>
2016-05-19 10:16:40 -0700

    update unitests for LA4IceReader. bug 32627

--- tests/unit/test_LA4IceReader.py
+++ tests/unit/test_LA4IceReader.py
@@ -6,13 +6,13 @@ from pbtranscript.io import BLASRRecord, LA4IceReader
 from test_setpath import DATA_DIR, OUT_DIR
 
 
-t0_qaln = """agaaacca-aga-agagaaagacaga-gaagagaacagaaaagaatacagaataggagagaaaaagagagagagacagagag-agaga-agaagag-ag-gaa-gaga-agata-gagagagaa-agaaa-gagga--ga-gagaagagagataa-acga-gagg-gagaagagagaagag-agaagagaacagaga-agagagaaaaaatgta-agagaagagagatagaaaa-aag-gagagat-a-gacgaa-g-aga-gagacagagaagag-agaacagatagaagagaatagagagaca-agaatgga-agcag-gaccgcg-gaacacgc--ggag"""
-t0_aaln = """|||*||*|*|||*|*||||*||*|||*||||||||**||||*|||*|*||||*||*|||||||||||||||||||*|*|||**|||||*|||||||*||*|||*||||*|||*|*|||||||||*|||||*||*||**||*|||||*|||||*||*|*||*|||**||||*||||*|||||*|*||||*||*|||||*||*|||||**||*|*|*|*||||||*|||*|||*||*|||*||||*|**|*|||*||*|*|||*||||*||||||*||*||||*|||*|*|||||||*|||||||*|*||||*|*|*||*||*||*||*|*|||*||*|**||||"""
-t0_saln = """agagac-agagagacagaacgaaagaagaagagaa--gaaa-gaagagagaagag-agagaaaaagagagagagagaaagaacagagacagaagaggagagaaagagagagaaaagagagagaagagaaacgaagacagaagagaacagaga-aagaagaagagaagaga-gagacaagagcaaaaga-aagagagagagcgagaaggaaag-agaaagaagaaagaaagacaagaagagagacaagaagacaaaaggagaagagagagagaa-aggagaaaaga-aaaagagaaaagagagagacagaa-gcagag-agagaacg-gagaagac-caaggag"""
+t0_qaln="""ctatgagtaaat-atacta-gtata--a-atacga-ata-a--ata--tatctatagtatatataga--taga-tata-tag-agtatag-aata-aata-gta-ataatataagtata-tatatatagcta-a-tataatatactaata-gtaaaata-tactata-agtatatatactaagtatatatactatagtagtatataa-c-atagtagatata-ta-ta-atatatagtata-taatatatagtatagtata-taata-gtatatatactatagta-taataactatata-tgtatata-tata-gtatataggtataaata-gtataatatcccat-gtactct"""
+t0_aaln="""|*|*||||***|*||||*|*|*|*|**|*||**||*|||*|**|||**|||||||||||||||*|||**||*|*||||*||**|*|||||*||||*||||*|||*|||||||||*||||*||||||||*|||*|*||||*|||||||*||*|||*|*||*|||||||*||||*|||||**||**|||||||*||**|||*|||||||*|*||||||*|||||*||*||*|||||||*||||*||*||||||*||*||||||*||*||*|||||||||||||||||*||*||*|||*|||*|*||||||*||||*|||*|||**||||*|||*|||||*|||***||*|||*|*|"""
+t0_saln="""c-a-gagt-ggtgatacaacgcagagtacatgggatatatactatagatatctatagtatata-agaacta-agtataataatactatagtaatataatatgtatataatataactatagtatatata-ctatagtata-tatacta-taagtata-taatactatatagta-atata--aa--atatatagta--gta-tatataatctatagtatatatactaatatatatatactatagta-tatata-taaagtatacta-tatgtatatatactatagtaata-ta-cta-atagtatatatactataagta-ata--tatatataagtata-tatagtatagtaatat"""
 
-t1_qaln = """cag-agtggtgatacaacgcagagtacatgggatatatactataga-ta--t-c-ta-tagtatataag-a-actaagtataata-ata-cta-tagtaatataa-tat-gtatataatataactatagta-tatatactatagtatatatactataagtatataatactatatagtaatataaaata-tatag-ta-gtatatata-atctatagtatatatac-taatatatatatactatagtatatata-taa-agtatac-tatatgtatatata-cta-tagtaatatactaatagtata-tatactataagtaatatatatataagtatatatagtatagtaatatat--c-tagata-atatactatagtatata-taactagtagtaatagtaataagtatatatataagtatatatactatagtata-taa-tatagata-atatagtaaa-tatcccatgtactctgc-gtgtgataccac-tg--ct-t"""
-t1_aaln = """*||*||||||*||*||||||||||||||||||||||||||||||||*||**|*|*||*||*|||||||**|*|*||*|||||*||*|||*|||*|||||||||||*|||*|||||||*||||*|||*|*||*||*|||**||||*|||*|||*||*||*||*|||*||||||**|||||*|||*|*||*||||**||*|*|||*|||*||||||||||||||||**||**|||||||*|*|||||*|||*|||*|||*|*||||**|||||**|||||||*|||*||*||||||||||||||||*|*|||||||||||||*||||||**||||||||||||*|||||||*|||||**|*|||*||*|||||*||||*||||||*||**||*||*||*|||||*||*|||||||*|||*|||||||||||||||||||*|||*||||*||**|||||*||*|*|||***|*|||*|*****||*|*|||*|*|*||**||*|"""
-t1_saln = """aagcagtggt-at-caacgcagagtacatgggatatatactatagaataaatacatagtaatatataaatataatacgtatagtatataactaatagtaatataactatagtatata-tata-cta-a-taatagata-aatag-ataaatagta-aa-tagatattactat-gagtaa-atatactagtataaatacgaataaatatatctatagtatatatagata-gatatatagagtatagaataaatagtaataatataagtatat--atatatagctaata-taatatactaatagtaaaatatactataagta-tatata-ctaagtatatatactatagtagtatataacatag-tagatata-tataatatatagtatataata-ta-tagta-ta-gtatata-ata-gtatatatactatagtataataactatatatgtatata-tatagtat-ataggta-taaatagtataatatcccatgtactct"""
+t1_qaln="""aagcagtggt-at-caacgcagagtacatgggatatatactatagaataaatacatagtaatatataaatataatacgtatagtatataactaatagtaatataactatagtatata-tata-ctaataatagata-a--atag-ataaatagta-aa-tagatattactatg-agtaaatatactagtataaa-tacgaataaatatatctatagtatatatagatag-atatatagagtatagaataaatagtaataatataagtatatatatatag-ctaata-taatatactaatagtaaaatatactataagta-tatatac-taagtatatatactatagtagtatataacatag-tagatata-tataatatatagtata-taata-ta-tagta-ta-gtatata-ata-gtatatatactatagtataataactatatatgtatata-tatagtatat-aggtataaata-gtataatatccca-tg-ta"""
+t1_aaln="""*||*||||||*||*||||||||||||||||||||||||||||||||*||**|*|*||*||*|||||||**|*|*||*|||||*||*|||*|||*|||||||||||*|||*|||||||*||||*|||*||*||*|||*|**||||*|||*|||*||*||*||*|||*||||||**|||||*||||**|*||||*|*||*|*|||*|||*||||||||||||||||**||**|||||||*|*|||||*|||*|||*|||*|*||||**|||||*||||||**|||*||*||||||||||||||||*|*|||||||||||||*||||||**||||||||||||*|||||||**|||||*|*|||*||*|||||*||||*||||||*||*|*||*||*||*|||||*||*|||||||*|||*|||||||||||||||||||*|||*||||*||**|||||*||*|*|||***|*|||****|**||*|*|||*||*|*||*|*"""
+t1_saln="""cag-agtggtgatacaacgcagagtacatgggatatatactataga-ta--t-c-ta-tagtatataa-ga-actaagtataata-ata-cta-tagtaatataa-tat-gtatataatataacta-tagtatatatactatagtatatatactataagtatataatactatatagtaa-tata-aaatatatagta-gtatatata-atctatagtatatata-ctaatatatatatactatagtatatata-taa-agtata-ctatatgtatatatacta-tagtaatatactaatagtata-tatactataagtaatatatatataagtatatatagtatagta--atatatc-tagata-atatactatagtatata-ta-actagtagtaatagtaataagtatatatataagtatatatactatagtata-taa-tatagat-aatatagta-aatatcccatgta-ctctgcgtgtgata-cc-actgctt"""
 
 
 class TEST_LA4ICEREADER(unittest.TestCase):
@@ -22,23 +22,27 @@ class TEST_LA4ICEREADER(unittest.TestCase):
         self.dataDir = DATA_DIR
         self.outDir = OUT_DIR
         self.las_out = op.join(self.dataDir, "test_LA4IceReader.las.out")
+        # las_out was generated as follows:
+        #   create reads.fasta containing four reads (t0_qaln, t0_saln, t1...)
+        #   call 'ice_daligner.py reads.fasta reads.fasta out'
+        #   copy the first two records in out/*.N1.las.out and stop signs to las_out
         self.t0 = BLASRRecord(
-            qID=884,
-            qLength=308, qStart=0, qEnd=308, qStrand='+',
-            sID=472,
-            sLength=410, sStart=74, sEnd=402, sStrand='+',
-            score=-328, mapQV=None,
+            qID=4,
+            qLength=472, qStart=158, qEnd=472, qStrand='+',
+            sID=3,
+            sLength=461, sStart=0, sEnd=327, sStrand='+',
+            score=-327, mapQV=None,
             qAln=t0_qaln, alnStr=t0_aaln, sAln=t0_saln,
-            strand='+', identity=72.64)
+            strand='+', identity=72.23)
 
         self.t1 = BLASRRecord(
-            qID=1472,
-            qLength=463, qStart=2, qEnd=463, qStrand='+',
-            sID=480,
-            sLength=489, sStart=0, sEnd=472, sStrand='+',
-            score=-472, mapQV=None,
+            qID=4,
+            qLength=472, qStart=0, qEnd=468, qStrand='+',
+            sID=3,
+            sLength=461, sStart=0, sEnd=461, sStrand='+',
+            score=-461, mapQV=None,
             qAln=t1_qaln, alnStr=t1_aaln, sAln=t1_saln,
-            strand='+', identity=74.92)
+            strand='+', identity=75.03)
 
 
     def test_LA4IceReader(self):
